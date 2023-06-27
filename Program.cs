8a80344a7e0077443c352bd20a90a2ae2c308aa7
Michael Khanda <michaelkhanda006@gmail.com>
2023-06-28 00:05:50 +0300

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Methodswitharrays
+{
+    class Program
+    {
+        static int Addition(int[]x)
+        {
+            int total = 0;
+            for(int i=0; i<5; i++)
+            {
+                total = total + x[i];
+            }
+            return total;
+        }
+        
+
+        static void Main(string[] args)
+        {
+            int[] x = { 90, 89, 78, 45, 74 };
+            int sum = Addition(x);
+            Console.WriteLine("The sum is:" + " " + sum);
+        }
+    }
+}
